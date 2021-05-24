4e3c104572ffe80846d93ea417742122c903f8fc
unknown <kacper.szwarga17@gmail.com>
2021-05-24 11:32:35 +0200

    Zaj 9

--- google/autocompleter.js
+++ google/autocompleter.js
@@ -1,134 +1,105 @@
 Vue.component('v-autocompleter', {
-    template: `       
-    
-                                    <input v-autocompleter class ="search-searchbar-input" type="search" v-model="googleSearch" @focus="focused = true" @keyup.enter="enter()" @keyup.down="down()" @keyup.up="up()"></input>
+  template: `                   
 
-                          
+  <div>
+    <input class="google-text-input" :value="value" @input="$emit('input', $event.target.value)" type="search"
+        v-on:click="ustaw()" ref="first" @focus="chosen=true" @keyup.down="down()" @keyup.up="up()"
+        @keyup.enter="enter()" />
+    <div>
+        <ul
+            :class="[value.length !== 0 && chosen && filteredCities.length !== 0 ? '' : 'nothing']">
+            <li :class="{current: current === index}" v-for="(city, index) in filteredCities"
+                v-on:click="mouseClick(city.name)">
+                <div class="city" v-html="Bold(city.name)"></div>
+            </li>
+        </ul>
+    </div>
+  </div>               
     `,
-    data: {
-            googleSearch: "",
-            cities: window.cities,
-            fraza: "",
-            results: "",
-            current: -1,
-            update_filteredCities:true,
-            focused: false,
-            change: false,
-        },
-    watch: {
+  props: ['options', 'value'],
 
-            current : function() {
-            this.update_filteredCities=false;
-            this.googleSearch = this.filteredCities[this.current].name;
-            },
-            googleSearch: function(){
-                this.FilteredCities(this.update_filteredCities);
-                this.update_filteredCities=true;
-                console.log(this.filteredCities);
-
-                if(this.current==-1){
-                    this.searchedInput=this.googleSearch;
-                    
-                }
-            }
-
-    },
-    methods: {
-        autocomplete(city) {
-            this.googleSearch = city.name;
-            this.results = this.googleSearch;
-        },
-        enter() {
-            this.update_filteredCities=true;
-            this.change= true;
-            this.current=-1;
-         },
-        Bold: function(x)
-        {
-            
-            return x.replaceAll(this.googleSearch, '<span class="light">' + this.googleSearch + '</span>')
-              
-        },
-    
-        down: function() {
-            if (this.current < this.filteredCities.length)
-            {
-                this.current++;
-            } else if (this.current == this.filteredCities.length)
-            {
-                this.current = 0;
-            }
-        },
-
-        up: function() {
-            if(this.current > 0)
-            {
-                this.current--;
-            } else if (this.current < 0)
-            {
-                this.current = this.filteredCities.length - 1;
-            }
-        },
-        currentCity: function() {
-            this.googleResults = true;
-        },
-
-        FilteredCities(yes){
-            if(yes){
-               let result=this.cities.filter(city => city.name.includes(this.googleSearch));
-               if(result.length>10){
-                   this.filteredCities= result.slice(1, 11);
-               }
-               else{
-                   this.filteredCities= result;
-               }
-               this.current=-1;
-            }   
-        },
-        selected(i){
-            this.googleSearch=this.filteredCities[i].name;
-            this.enter();
-         },
-}
-})
-
-var app = new Vue({
-        el: '#app',
-        data: {
-            googleSearch: "",
-            cities: window.cities,
-            fraza: "",
-            results: "",
-            current: -1,
-            update_filteredCities:true,
-            focused: false,
-            change: false,
-        },
-        updated() {
-            this.$nextTick(() => {
-
-                if (this.change) {
-                    this.$refs.top.focus();
-
-                } else {
-
-                    this.$refs.bottom.focus();
-
-                }
-
-        })},
-    
-
-        methods: {
-
-
-             Results(){
-                if(this.googleSearch.length==0){
-                    this.change=false;
-                }
-                return this.change;
-
-             }
+  data: function () {
+    return {
+      cities: window.cities,
+      filteredCities: "",
+      update_filteredCities: true,
+      chosen: false,
+      current: -1,
+      searchedInput: ''
     }
-    });
+  },
 
+  watch: {
+
+    current: function (newValue) {
+      if (newValue < 0) {
+        return;
+      }
+      this.update_filteredCities = false;
+
+      this.$emit('input', this.filteredCities[this.current].name);
+    },
+
+    value: function () {
+      this.FilteredCities(this.update_filteredCities);
+      this.update_filteredCities = true;
+      console.log(this.filteredCities);
+
+      if (this.current == -1) {
+        this.searchedInput = this.value;
+      }
+    }
+  },
+  methods: {
+
+    mouseClick: function (a) {
+      this.$emit('input', a); 
+      this.update_filteredCities = true;
+      this.chosen = true;
+      this.current = -1;
+      this.$emit('enter', this.value);
+    },
+
+    Bold: function (x) {
+      result = this.value;
+      return x.replaceAll(result, '<span class="Bold">' + result + '</span>')
+    },
+
+
+    enter: function () {
+      this.update_filteredCities = true;
+      this.chosen = false;
+      this.current = -1;
+      this.$emit('enter', this.value);
+    },
+
+    down: function () {
+      if (this.current < this.filteredCities.length - 1) {
+        this.current++;
+      } else if (this.current == this.filteredCities.length - 1) {
+        this.current = 0;
+      }
+    },
+
+
+    up: function () {
+      if (this.current > 0) {
+        this.current--;
+      } else if (this.current == 0) {
+        this.current = this.filteredCities.length - 1;
+      }
+    },
+
+    FilteredCities: function (bool) {
+      if (bool) {
+        let result = this.cities.filter(city => city.name.includes(this.value));
+        if (result.length > 10) {
+          this.filteredCities = result.slice(1, 11);
+        } else {
+          this.filteredCities = result;
+        }
+        this.current = -1;
+      }
+    }
+  }
+});
